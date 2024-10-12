93e0c4cc436459f03b7742389d015ef55230dd26
shree081297 <srikanthkurella123@gmail.com>
2024-10-12 20:33:54 +0530

    Fourth commit

--- src/test/java/stepdefs/TestRunner.java
+++ src/test/java/stepdefs/TestRunner.java
@@ -6,20 +6,17 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		monochrome=true,
-		plugin= {"pretty"},
-		features="src/test/java/Features",
-		glue="stepdefs",
-		//tags= {"not @Ignore"}--to ignore the tag
-		//tags={"@smoke"}//--to run only one tag name testcase
-		//tags={"@calculator"}--to run on class level
-	//	tags= {"@Simplilearn and not @Ignore"}--run every testcase in feature file except ignored one
-		tags= {"@regression"}//--run only regression test case
-		//tags= {"@Simplilearn"}
-		
-		)
+@CucumberOptions(monochrome = true, plugin = { "pretty", "html:target/html-cucumber",
+		"json:target/cucumber.json" }, features = "src/test/java/Features", glue = "stepdefs",
+		// tags= {"not @Ignore"}--to ignore the tag
+		// tags={"@smoke"}//--to run only one tag name testcase
+		// tags={"@calculator"}--to run on class level
+		// tags= {"@Simplilearn and not @Ignore"}--run every testcase in feature file
+		// except ignored one
+		tags = { "@regression" }// --run only regression test case
+// tags= {"@Simplilearn"}
 
+)
 
 public class TestRunner {
 
