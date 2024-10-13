a298a04cbd147650d14059abb6168cb272f28e99
shree081297 <srikanthkurella123@gmail.com>
2024-10-13 20:32:00 +0530

    added jenkins file

--- src/test/java/stepdefs/TestRunner.java
+++ src/test/java/stepdefs/TestRunner.java
@@ -6,14 +6,16 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, plugin = { "pretty", "html:target/html-cucumber",
-		"json:target/cucumber.json" }, features = "src/test/java/Features", glue = "stepdefs",
+@CucumberOptions(monochrome = true,
+				 plugin = { "pretty", "html:target/html-cucumber","json:target/cucumber.json" },
+				 features = "src/test/java/Features", 
+				 glue = "stepdefs",
 		// tags= {"not @Ignore"}--to ignore the tag
 		// tags={"@smoke"}//--to run only one tag name testcase
 		// tags={"@calculator"}--to run on class level
 		// tags= {"@Simplilearn and not @Ignore"}--run every testcase in feature file
 		// except ignored one
-		tags = { "@regression" }// --run only regression test case
+		         tags = { "@regression" }// --run only regression test case
 // tags= {"@Simplilearn"}
 
 )
